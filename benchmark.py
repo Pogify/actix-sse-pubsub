736072818c3b03f72ccfd0b923be260ee492f710
r2dev2bb8 <ronak.badhe@gmail.com>
2020-08-19 04:17:39 -0700

    added benchmark script

---
+++ benchmark.py
@@ -0,0 +1,46 @@
+from sseclient import SSEClient
+from time import sleep
+from threading import Lock, Thread
+
+class Counter(Thread):
+    def __init__(self):
+        super().__init__()
+        self.daemon = True
+        self.counter = 0
+        self.connections = []
+        self.lock = Lock()
+
+    def increment(self, connection):
+        with self.lock:
+            self.counter += 1
+            # self.connections.append(connection)
+            print(self.counter, end='\r')
+
+counter = Counter()
+counter.start()
+
+def task(counter):
+
+    try:
+        messages = SSEClient("http://localhost:8080/events/channel1")
+    except OSError:
+        return
+    for msg in messages:
+        if str(msg) == "connected":
+            counter.increment(messages)
+        if "secret" in str(msg):
+            counter.increment(messages)
+
+
+threads = []
+for _ in range(10000):
+    t = Thread(target=task, args=(counter,), daemon=True)
+    t.start()
+    threads.append(t)
+
+try:
+    for t in threads:
+        t.join()
+except KeyboardInterrupt:
+    exit()
+
